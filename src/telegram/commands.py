204ad836067f3245a17801f2869672739b065819
zangico <zangico@gmail.com>
2025-10-26 09:33:54 -0400

    feat: add possibility to get chat_id directly in chat

--- src/telegram/commands.py
+++ src/telegram/commands.py
@@ -3,9 +3,14 @@
 async def start(bot, chat_id: int):
     await bot.send_message(chat_id, "Welcome to MacroBot")
 
+async def get_chat_id(bot, chat_id:int):
+    await bot.send_message(chat_id, f"Your chat id is: {chat_id}")
+
 
 async def sort_commands(bot, chat_id: int, command: str):
     if command == "start":
         return await start(bot, chat_id)
+    elif command == "chat_id":
+        return await get_chat_id(bot, chat_id)
     
     return command
